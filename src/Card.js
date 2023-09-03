d32f0ea5ea033ed7c1a5b884f48793b00a9897f9
Eiji971 <bosc_emery@outlook.com>
2023-09-02 21:39:44 -0400

    text changes, add img

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 import React from "react"
 import bg from './bg.png';
+import slime from "./slime.png"
 export default function Card(props) {
     /*
     const [connectedUser, setConnectedUser] = useState(""); // State to store the connected username
@@ -19,20 +20,23 @@ export default function Card(props) {
         return (
             <div className="card">
                 <img src={bg} className="bg--img" alt="Background" />
-                <div className="card--stats">
+                <div className="card--mainstats">
                     <span className="main--infos">{props.item.username}</span>
                     <span className="main--infos"> lvl {props.item.player.level}</span>
                 </div>
                 <div>
                     <span className="submain--infos">{props.item.gold}</span>
                 </div>
-                <div>
-                    <span className="sub--infos">Vitality: {props.item.player.permaStats.vitality}</span>
-                    <span className="sub--infos">Strength: {props.item.player.permaStats.strength}</span>
-                    <span className="sub--infos">Agility: {props.item.player.permaStats.agility}</span>
-                    <span className="sub--infos">Intelligence: {props.item.player.permaStats.intelligence}</span>
-                    <span className="sub--infos">Resistance: {props.item.player.permaStats.resistance}</span>
-                    <span className="sub--infos">Luck: {props.item.player.permaStats.luck}</span>
+                <div >
+                    <img src={slime} className="slime--img" alt="char" />
+                </div>
+                <div className="sub--infos--container">
+                    <span className="sub--infos">VIT:{props.item.player.permaStats.vitality}</span>
+                    <span className="sub--infos">STR:{props.item.player.permaStats.strength}</span>
+                    <span className="sub--infos">AGI:{props.item.player.permaStats.agility}</span>
+                    <span className="sub--infos">INT:{props.item.player.permaStats.intelligence}</span>
+                    <span className="sub--infos">RES:{props.item.player.permaStats.resistance}</span>
+                    <span className="sub--infos">LCK:{props.item.player.permaStats.luck}</span>
                 </div>
                 <div>
                     {props.item.player.weapon && props.item.player.weapon.name !== null ? (

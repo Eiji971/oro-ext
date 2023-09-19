b2e4b131dd75e01433ae17c25a9db905fee988c6
Eiji971 <bosc_emery@outlook.com>
2023-09-19 15:20:40 -0400

    change to newer syntax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const root = document.getElementById('root'); // Get the root DOM element
+
+const reactRoot = createRoot(root); // Create a root for your React app
+
+reactRoot.render(<App />); // Render your app inside the root
